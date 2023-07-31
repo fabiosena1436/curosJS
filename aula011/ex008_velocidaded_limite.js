ab24c93b8505dca81c451a94d02395e7995648b7
Fabio <128174066+fabiosena1436@users.noreply.github.com>
2023-07-31 16:32:01 -0300

    ex javascript

---
+++ aula011/ex008_velocidaded_limite.js
@@ -0,0 +1,13 @@
+var vel = 70
+var vel_limit = 50
+
+console.log(`A velocidade do seu carro é ${vel} km/h`)
+if (vel > 50) {
+    console.log(`Voce ultrapassou a velocidade permitida de ${vel_limit}km/h Voce foi multado `)
+    
+} else{
+    
+        console.log(`Acima de ${vel_limit}km/h tome muito cuidado voce pode ser multado`)
+    
+}
+
