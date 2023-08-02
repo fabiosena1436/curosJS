899e483b65a500f041b5b9cf71b2e0d69b688c91
Fabio <128174066+fabiosena1436@users.noreply.github.com>
2023-08-02 14:07:46 -0300

    ex

---
+++ aula012/ex011votaçao.js
@@ -0,0 +1,16 @@
+//let idade = 18
+//   if (idade < 18)  {
+//       console.log(`voce é menor de idade`)
+//   }else {
+//       console.log(`voce e maior de idade`)
+//   }
+
+var idade = 70
+console.log (`voce tem ${idade} anos`)
+if (idade < 16) {
+    console.log(`nao vota`)
+} else if (idade < 18 || idade > 65) {
+    console.log(`voto opcional`)
+} else {
+    console.log('voto obrigatório')
+}
