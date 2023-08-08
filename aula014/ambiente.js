bd29912c446578858117abfad26aa91889b450bc
Fabio <128174066+fabiosena1436@users.noreply.github.com>
2023-08-08 16:17:25 -0300

    continuaçao com repetiçao javascript

---
+++ aula014/ambiente.js
@@ -0,0 +1,11 @@
+/*var c = 1
+while(c <= 6) {
+    console.log('tudo bem')
+    c++
+}
+*/
+console.log('vai começar a contagem')
+for (var c = 1; c <= 6; c++) {
+    console.log(c)
+}
+console.log ('ja acabou')
